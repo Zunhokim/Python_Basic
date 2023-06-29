8492e27eaf4c6fcbbc382d92c198c807b7013467
Zunhokim <zxcrtf245@naver.com>
2023-06-29 15:56:32 +0900

    __all__

--- Lecture_Note/40_packageuse.py
+++ Lecture_Note/40_packageuse.py
@@ -4,10 +4,17 @@
 # trip_to.detail()
 
 #from () import () 구문에서는 ~~에서 ~~함수를 import 하는 것이므로 바로 사용이 가능함. 
-from travel.thailand import ThailandPackage
-trip_to = ThailandPackage()
-trip_to.detail()
+# from travel.thailand import ThailandPackage
+# trip_to = ThailandPackage()
+# trip_to.detail()
 
-from travel import vietnam
+# from travel import vietnam
+# trip = vietnam.VietnamPackage()
+# trip.detail()
+
+from travel import * # travel 폴더 안의 모든 페키지를 가져오곘음
+
 trip = vietnam.VietnamPackage()
 trip.detail()
+trip2 = thailand.ThailandPackage()
+trip2.detail()
