17f5978f4c6e669f8b97a7f90c45b6d8c5f2d737
Zunhokim <zxcrtf245@naver.com>
2023-06-29 15:51:42 +0900

    Basic Thesis of Package

---
+++ Lecture_Note/40_packageuse.py
@@ -0,0 +1,13 @@
+#import 구문에서는 클래스 내의 함수를 바로 사용이 불가능함. 
+# import travel.thailand
+# trip_to = travel.thailand.ThailandPackage()
+# trip_to.detail()
+
+#from () import () 구문에서는 ~~에서 ~~함수를 import 하는 것이므로 바로 사용이 가능함. 
+from travel.thailand import ThailandPackage
+trip_to = ThailandPackage()
+trip_to.detail()
+
+from travel import vietnam
+trip = vietnam.VietnamPackage()
+trip.detail()
