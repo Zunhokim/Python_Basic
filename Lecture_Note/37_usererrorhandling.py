263b1d11467aabf00a0aeb7c2086bfd79ef13df2
Zunhokim <zxcrtf245@naver.com>
2023-06-29 14:05:58 +0900

    user error handling

---
+++ Lecture_Note/37_usererrorhandling.py
@@ -0,0 +1,19 @@
+class BigNumberError(Exception):
+    def __init__(self, msg):
+        self.msg = msg
+        
+    def __str__(self):
+        return self.msg
+
+try:
+    print("한 자리 숫자 나누기 전용 계산기")
+    num1 = int(input("num1 : "))
+    num2 = int(input("num2 : "))
+    if num1 >= 10 | num2 >= 10:
+        raise BigNumberError("Input : {0} / {1}".format(num1, num2))
+    print("{0} / {1} = {2}".format(num1, num2, int(num1/num2)))
+except ValueError as err:
+    print(err)
+
+except BigNumberError as err:
+    print(err)
